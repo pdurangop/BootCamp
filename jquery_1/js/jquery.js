a811b53e9069d80cb4e14161d629f2f67e0ff9ac
pdurangop <pdurangop@gmail.com>
2016-06-29 10:42:56 -0500

    primer reto de Jquery listo

---
+++ jquery_1/js/jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('button').on('click', function() {
+  var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
+  $('.usa').append(message);
+  $('button').remove();
+});});
